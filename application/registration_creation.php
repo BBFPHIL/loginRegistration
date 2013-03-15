29c21c8300c995771c9ae0e58173a179859f7be3
Phillip Walker <pwalker@fordham.edu>
2013-03-15 18:09:39 -0400

    Restructured filter var statement

--- application/registration_creation.php
+++ application/registration_creation.php
@@ -14,50 +14,56 @@ if(isset($_POST['username']) && !empty($_POST['password']) &&
 isset($_POST['password']) && !empty($_POST['password']) && 
 isset($_POST['email_address']) && !empty($_POST['email_address']) &&
 isset($_POST['first_name']) && !empty($_POST['first_name']) &&
-isset($_POST['last_name']) && !empty($_POST['last_name']) &&
-filter_var($_POST['email_address'], FILTER_VALIDATE_EMAIL)){
+isset($_POST['last_name']) && !empty($_POST['last_name'])){
 
-
-	$username=$_POST['username'];
-	$password=$_POST['password'];	
-	$username=$_POST['first_name'];
-	$username=$_POST['last_name'];
-	$username=$_POST['email_address'];
-	
-	//check if email exists
-	
-	$stmt = $db->conn->prepare("SELECT * FROM membership WHERE email_address=:email_address");
-	
-	$stmt->bindParam(":email_address", $email_address , PDO::PARAM_STR);
-	
-	$stmt->execute();
-	
-	$result = $stmt->fetch(PDO::FETCH_ASSOC);
-	
-	
-	if($stmt->rowCount() == 0){
-	
+	if(!filter_var($_POST['email_address'], FILTER_VALIDATE_EMAIL)){
 		
-		//prepare for query
-		$stmt = $db->conn->prepare("INSERT INTO membership (first_name, last_name, username, password, email_address) VALUES (:first_name, :last_name, :username, :password, :email_address)");
-	
-		$stmt->bindParam(":first_name", $first_name , PDO::PARAM_STR);
-		$stmt->bindParam(":last_name", $last_name , PDO::PARAM_STR);		
-		$stmt->bindParam(":username", $username , PDO::PARAM_STR);
-		$stmt->bindParam(":password", $password , PDO::PARAM_STR);
+		echo "Please enter a valid email";
+		
+	}else{
+		
+
+		$username=$_POST['username'];
+		$password=$_POST['password'];	
+		$username=$_POST['first_name'];
+		$username=$_POST['last_name'];
+		$username=$_POST['email_address'];
+		
+		//check if email exists
+		
+		$stmt = $db->conn->prepare("SELECT * FROM membership WHERE email_address=:email_address");
+		
 		$stmt->bindParam(":email_address", $email_address , PDO::PARAM_STR);
 		
 		$stmt->execute();
 		
 		$result = $stmt->fetch(PDO::FETCH_ASSOC);
 		
-		echo "Congratulations, you have signed up!";
 		
-	}else{
+		if($stmt->rowCount() == 0){
 		
-		echo "Email already exists, please pick a different email";
+			
+			//prepare for query
+			$stmt = $db->conn->prepare("INSERT INTO membership (first_name, last_name, username, password, email_address) VALUES (:first_name, :last_name, :username, :password, :email_address)");
+		
+			$stmt->bindParam(":first_name", $first_name , PDO::PARAM_STR);
+			$stmt->bindParam(":last_name", $last_name , PDO::PARAM_STR);		
+			$stmt->bindParam(":username", $username , PDO::PARAM_STR);
+			$stmt->bindParam(":password", $password , PDO::PARAM_STR);
+			$stmt->bindParam(":email_address", $email_address , PDO::PARAM_STR);
+			
+			$stmt->execute();
+			
+			$result = $stmt->fetch(PDO::FETCH_ASSOC);
+			
+			echo "Congratulations, you have signed up!";
+			
+		}else{
+		
+			echo "Email already exists, please pick a different email";
 	
-	}
+		}
+	}//close filter var
 	
 }else{
 
